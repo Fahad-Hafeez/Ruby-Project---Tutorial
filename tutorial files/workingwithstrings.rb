d585ff31b975fe4668cc635800b34bf4e8b492a8
Fahad-Hafeez <79376608+Fahad-Hafeez@users.noreply.github.com>
2021-06-24 19:24:31 +0100

    examples of how to write strings and how to syntax them

---
+++ tutorial files/workingwithstrings.rb
@@ -0,0 +1,23 @@
+# Declating phrase/phrases
+phrase  = "Giraffe Academy"
+phrase  = "   Giraffe Academy   "
+# How to print out the above phrase in uppercase
+puts phrase.upcase()
+# How to print out above phrase in lowercase
+puts phrase.downcase()
+# Prints the length of the phrase
+puts phrase.length()
+# Checks if a certain word is in the phrase; gives answer in true or false
+puts phrase.include? "Academys"
+# Below is example of shortening a phrase with gaps or etc
+puts phrase.strip()
+# String below can be used to find the letter that goes with the index number
+puts phrase[]
+# Tells what position a specific character in string starts at
+puts phrase.index()
+# Certain phrase in the "" and gives the word/phrase in capital letters
+puts "programming".upcase()
+
+puts "Giraffe\nAcademy"
+puts "Girrafe" "Academy"
+puts "Girrafe Academy"
