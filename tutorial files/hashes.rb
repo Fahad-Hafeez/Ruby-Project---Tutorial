5617243ab03d03ca8f91bef02d36ed65f22b4dfe
Fahad-Hafeez <79376608+Fahad-Hafeez@users.noreply.github.com>
2021-06-25 22:25:23 +0100

    hashes and the structure

---
+++ tutorial files/hashes.rb
@@ -0,0 +1,12 @@
+# Hashes: wird is the key and defifinition is value basically a dictionary
+
+# Give hash a name and creae it and enter values
+states = {
+    "Pennsylvania" => "PA",
+    "New York" => "NA",
+    "Oregon" => "OR"
+}
+
+# print out the hash, by putting the value in speech marks
+# inside the square brackets or just copy the key in the hash
+puts states[]
