3e728b1c0d3a9957812d9c01bd3ceb1cc5bc1344
Fahad-Hafeez <79376608+Fahad-Hafeez@users.noreply.github.com>
2021-06-25 18:41:57 +0100

    Getting user input sample

---
+++ tutorial files/gettinguserinput.rb
@@ -0,0 +1,6 @@
+#To get user input, first of all you need to install platformio-ide-termina
+#Once thats done then there should be + sign, click that and it will open a new #termianl and thenyou can use any command there and get output. the one below #is an example.
+
+puts "Enter Your Name: "
+name = gets
+put ("Hello" + name)
