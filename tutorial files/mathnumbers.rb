9912a41a2bf7133fc9ace0de4f3e17ddb3614a97
Fahad-Hafeez <79376608+Fahad-Hafeez@users.noreply.github.com>
2021-06-25 18:34:01 +0100

    Syntax to do simple math calculations in Ruby

---
+++ tutorial files/mathnumbers.rb
@@ -0,0 +1,33 @@
+# to use a number just types a number after puts, like below whether whole/decimal/negative
+puts ""
+# to plus 2 or more numbers, just add numbers instead of the ""
+puts "" + ""
+# to divide 2 or more numbers, just add numbers instead of the ""
+puts "" / ""
+# to minus 2 or more numbers, just add numbers instead of the ""
+puts "" - ""
+# to multiply 2 or more numbers, just add numbers instead of the ""
+puts "" * ""
+# to rays a number like for example 2 to the power 2, just add numbers instead of the ""
+puts "" ** ""
+# divide a number by a another and split out the remainder,just add numbers instead of the ""
+puts "" % ""
+# print out a vaiable, just add numbers instead of the ""
+num = ""
+puts num
+puts ("my fav num " + num)
+puts ("my fav num " + num.to_s
+puts "my fav num " + num
+puts num.abs()
+# rounds the number
+puts num.round()
+# give highest number in the vaiable range
+puts num.ciel()
+# get the decimal to be taken off the variable
+puts num.floor()
+# get square root
+puts Math.sqrt("")
+# get zero as number back
+puts Math.log("")
+
+# to get a answer as decimal add a decimal point with a zero or any other number after it to all numbers
