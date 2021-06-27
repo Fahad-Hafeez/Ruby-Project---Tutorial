1803556eb728907ffead6f548f3a2b1806df1525
Fahad-Hafeez <79376608+Fahad-Hafeez@users.noreply.github.com>
2021-06-27 18:55:31 +0100

    forloops syntax etc and sample

---
+++ tutorial files/forloops.rb
@@ -0,0 +1,14 @@
+
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+friends.each do |friend|
+    puts friend
+end
+
+for index in 0..5
+    puts index
+end
+
+6.times do |index|
+    puts index
+end
