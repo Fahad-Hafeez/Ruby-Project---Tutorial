5aaf663dde98ab314cb5d3a77b33258cad311d91
Fahad-Hafeez <79376608+Fahad-Hafeez@users.noreply.github.com>
2021-06-28 22:49:11 +0100

    Update quiz.rb

--- Quiz/quiz.rb
+++ Quiz/quiz.rb
@@ -26,5 +26,7 @@ def run_test(questions)
             score += 1
         end
     end
-    puts ("You got " + score + "/" + questions.length())
+    puts ("You got " + score.to_s + "/" + questions.length().to_s)
 end
+
+run_test(questions)
